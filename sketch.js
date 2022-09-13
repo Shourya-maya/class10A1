5038c7eff821e3e97403cd4c9726ba66d91d82d6
Shourya-maya <112942919+Shourya-maya@users.noreply.github.com>
2022-09-13 19:57:23 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var weight=[35,38,40,45] 
+var sum=0
+function setup() 
+{
+  createCanvas(400,400);
+sum=weight[0]+ weight[1]+weight[2]+weight[3]
+console.log(sum)
+var avg=(sum/weight.length)
+console.log(avg)
+}
+
+
+function draw() 
+{
+background(51);
+
+}
